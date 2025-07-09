de2730b96317f9fc91e8f1a40aab5cbd9d2ac2ac
Damir Rysaev <freepad@ya.ru>
2025-07-09 22:14:25 +0300

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css';
-import ShopItemFunc from './components/shopItemFunc/ShopItemFunc';
+import './App.css'
+import ShopItemFunc from './components/shopItemFunc/ShopItemFunc'
 
 function App() {
-
   const item = {
     brand: 'Tiger of Sweden',
     title: 'Leonard coat',
     description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
     price: 399,
-    currency: '£'
+    currency: '£',
   }
 
   return (
     <>
       <div className="container">
-        <div className="background-element">
-        </div>
+        <div className="background-element"></div>
         <div className="highlight-window">
-          <div className='highlight-overlay'></div>
+          <div className="highlight-overlay"></div>
         </div>
         <div className="window">
           <ShopItemFunc item={item} />
--- src/components/shopItemFunc/ShopItemFunc.jsx
+++ src/components/shopItemFunc/ShopItemFunc.jsx
@@ -1,22 +1,25 @@
-const ShopItemFunc = ( {item} ) => {
-  const { brand, title, description, descriptionFull, price, currency } = item;
+const ShopItemFunc = ({ item }) => {
+  const { brand, title, description, descriptionFull, price, currency } = item
 
   return (
     <div className="main-content">
       <h2>{brand}</h2>
       <h1>{title}</h1>
       <h3>{description}</h3>
-      <div className="description">
-        {descriptionFull}
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
       </div>
-      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{currency}{price.toFixed(2)}</div>
+        <div className="price">
+          {currency}
+          {price.toFixed(2)}
+        </div>
         <button>Добавить в корзину</button>
       </div>
     </div>
   )
 }
 
-export default ShopItemFunc;
+export default ShopItemFunc
